91168e3f820d8deac317d06000325f89b05db695
IGOR EVSSEV <andrey.semin.evseev@gmail.com>
2024-04-14 18:26:20 +0300

    создание api + вывода

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
+let block = document.querySelector(".block");
+let newDog = document.querySelector(".newdog");
+
 function getDog() {
-  fetch("https://random.dog/woof.json").then((data) => {
-    console.log(data);
-  });
+  fetch("https://random.dog/woof.json")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      block.innerHTML += `<img src="${data.url}" alt=""/>`;
+    });
 }
+
+getDog();
+
+newDog.addEventListener("click", () => {
+  getDog();
+});
